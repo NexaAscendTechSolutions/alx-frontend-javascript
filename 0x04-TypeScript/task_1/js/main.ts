3e1d826f68b8d7d0d3065016742b001c28aa7ebe
nexaascendtechsolutions <admin@nexaascendtechsolutions.co.ke>
2025-09-29 16:55:38 +0300

    fixing main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,13 +18,10 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-// Implementation of the function
-const printTeacher: printTeacherFunction = (
-  firstName: string,
-  lastName: string
-): string => {
+// Implementation of the function (✅ no destructuring)
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
-};
+}
 
 // Example usage
 console.log(printTeacher("John", "Doe")); // J. Doe
