ab26bd4464769157779f68c3becbf3b897190d94
nexaascendtechsolutions <admin@nexaascendtechsolutions.co.ke>
2025-10-01 14:29:55 +0300

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -44,13 +44,43 @@ class Teacher implements TeacherInterface {
 
 // Factory function
 export function createEmployee(salary: number | string): Director | Teacher {
-  if (salary < 500) {
+  if (typeof salary === "number" && salary < 500) {
     return new Teacher();
   }
   return new Director();
+}
+
+// Type predicate
+export function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+// Execute work function
+export function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+}
+
+// ✅ String literal type
+type Subjects = "Math" | "History";
+
+// Function teachClass
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  }
+  return "Teaching History";
 }
 
 // ✅ Example usage
 console.log(createEmployee(200)); // Teacher
 console.log(createEmployee(1000)); // Director
 console.log(createEmployee("$500")); // Director
+
+console.log(executeWork(createEmployee(200))); // Getting to work
+console.log(executeWork(createEmployee(1000))); // Getting to director tasks
+
+console.log(teachClass("Math")); // Teaching Math
+console.log(teachClass("History")); // Teaching History
